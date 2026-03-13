ad0cb64fee6c04728b1eaccc681384b44e28f29b
mekouar1999 <othman.mekouar99@gmail.com>
2026-03-13 16:58:26 +0000

    back end enxpress+bdd operationel

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/connectDb');
 
+connectDB();
 dotenv.config();
 const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/api/v1/auth/register", (req, res) => {
+    res.json("Register");
+});
 
 const port = process.env.PORT || 5000;
 
